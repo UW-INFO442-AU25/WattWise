50d592285f1a2bc5f1d2e1c492040b36de11f62f
echou0321 <choueri2@uw.edu>
2025-11-10 16:27:24 -0800

    combined homepage components into index.jsx to import into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
 import './index.css'
-import Navbar from './components/Navbar'
-import Welcome from './components/Welcome'
-import AboutUs from './components/AboutUs'
-import RentingVsHomeowning from './components/RentingVsHomeowning'
-import EnvironmentImpact from './components/EnvironmentImpact'
-import QuizLeadIn from './components/QuizLeadIn'
+import HomePage from './pages/index'
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Welcome />
-      <AboutUs />
-      <RentingVsHomeowning />
-      <EnvironmentImpact />
-      <QuizLeadIn />
-    </>
+    <HomePage />
   )
 }
 
